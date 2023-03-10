bf81887bc9c421c2fb500ef58825afd30f9aa282
SaatwikRishi <saatwik_s@mckinsey.com>
2023-03-10 13:46:44 +0530

    opt: add more validations for collections page

--- src/pages/CollectionsPage/index.jsx
+++ src/pages/CollectionsPage/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar'
 import Entries from '../../components/Entries'
+import { DataContext } from '../../context/DataContext'
 export default function CollectionsPage () {
+  const { collections } = useContext(DataContext)
+
   const { id } = useParams()
-  if (!id || id.length === 0) {
+  const existsInCollection = collections.find((collection) => collection.id === parseInt(id))
+  if (!id || id.length === 0 || !existsInCollection) {
     return <Navigate to='/' />
   }
+
   return <div className='flex'>
     <Sidebar selectedIndex={parseInt(id)} />
     <Entries currentCollection={parseInt(id)} />
